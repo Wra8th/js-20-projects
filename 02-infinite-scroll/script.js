93f7589e826687f81708da0cf9826467b49ad685
hijuliancode <hijuliansosa@protonmail.com>
2020-12-13 14:55:55 -0500

    02 Infinite Scroll: JavaScript To Display Data

--- 02-infinite-scroll/script.js
+++ 02-infinite-scroll/script.js
@@ -4,19 +4,58 @@ const COUNT = 10
 const API_KEY = config.API_KEY;
 const API_URL = `https://api.unsplash.com/photos/random/?client_id=${API_KEY}&count=${COUNT}`
 
+const imagesContainer = document.getElementById('image-container')
+const loader = document.getElementById('loader')
+
+let photosArray = []
+
+// Function to SetAttributes to DOM Element
+
+function setAttributes(element, attributes) {
+  for(const key in attributes) {
+    element.setAttribute(key, attributes[key])
+  }
+}
+
+// Create Elements for Links & Photos, add to DOM
+async function displayPhotos() {
+  photosArray.forEach((photo) => {
+
+    const item = document.createElement('a')
+    setAttributes(item, {
+      href: photo.links.html,
+      target: '_blank',
+    })
+    
+    const img = document.createElement('img')
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    })
+
+    item.appendChild(img)
+    imagesContainer.appendChild(item)
+  })
+}
 
 // Get Photos from Unsplash API
-
 async function getPhotos() {
   try {
     const response = await fetch(API_URL)
-    const data = await response.json()
-    console.log(data)
+    photosArray = await response.json()
+    console.log(photosArray)
+    displayPhotos()
   } catch (error) {
     console.log('error')
   }
 }
 
 // OnLoad
+// window.onload = function() {
+//   getPhotos().then((data) => {
+//     displayPhotos(data)
+//   })
+// }
 
 getPhotos()
